40a9cdce1663e33e5eab9984c8347bfd24441f26
Matt <m.bimpson@gmail.com>
2019-05-14 14:39:45 -0500

    Initial commit

---
+++ Services/IUsersService.cs
@@ -0,0 +1,11 @@
+﻿using JwtApi.Models;
+using System.Collections.Generic;
+
+namespace JwtApi.Services
+{
+    public interface IUsersService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+    }
+}
